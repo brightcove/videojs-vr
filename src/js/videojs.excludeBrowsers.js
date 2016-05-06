e7064570882323499b57e820c3c94eeed9f0f643
Marcela Rodriguez <arilaen@gmail.com>
2016-05-06 10:22:42 -0400

    Debugging browser array

--- src/js/videojs.excludeBrowsers.js
+++ src/js/videojs.excludeBrowsers.js
@@ -30,8 +30,8 @@ module.exports = function(vjs) {
   }
   vjs.plugin('excludeBrowsers', function(options) {
     var usingExcludedBrowser = false;
-    browsers = options && options.browsers ? options.browsers : [];
-    console.log('Excluded browsers', browsers);
+    browsers = (options && options.browsers) ? options.browsers : [];
+    console.log('Excluded browser list', browsers);
     browsers.forEach(function(browser) {
       if ((browser === 'ie' && detectIE()) || (browser === 'safari' && detectSafari()) ||
         (ua === browser)) {
